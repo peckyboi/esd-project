c0a6cd31507d571816676758f91c9a69ebb4e785
peckyboi2 <darrenpeckjoonkiat@gmail.com>
2026-03-16 14:39:16 +0800

    feat(review): created review repository to help provide methods that are needed by review later in services

---
+++ services/review-microservice/src/main/java/com/freelance/reviewservice/repository/ReviewRepository.java
@@ -0,0 +1,22 @@
+package com.freelance.reviewservice.repository;
+
+import com.freelance.reviewservice.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+/**
+ * Handles reading and writing review rows to db and also handles queries, helps compartmentalize SQL so servies stay clean
+ */
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    Optional<Review> findByIdAndIsDeletedFalse(Integer id);
+
+    boolean existsByOrderIdAndClientIdAndIsDeletedFalse(Integer orderId, Integer clientId);
+
+    List<Review> findByGigIdAndIsDeletedFalse(Integer gigId);
+
+    List<Review> findByFreelancerIdAndIsDeletedFalse(Integer freelancerId);
+
+    List<Review> findByClientIdAndIsDeletedFalse(Integer clientId);
+}
