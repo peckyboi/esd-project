72a5c0bc33ee8e991b71f9c2b58ab107e2a196aa
samairaa2024-hub <samaira.a.2024@smu.edu.sg>
2026-04-02 14:03:11 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GigDetailPage from "./pages/GigDetailPage";
+import PlaceOrderPage from "./pages/PlaceOrderPage";
+import PaymentPage from "./pages/PaymentPage";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/gig/:id" element={<GigDetailPage />} />
+      <Route path="/place-order" element={<PlaceOrderPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
     </Routes>
   );
 }
